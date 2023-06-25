93035199e297c3a1ee7a17eb3ee677e71d2930b1
Dimitri <65912918+dimitri12482@users.noreply.github.com>
2023-06-25 07:56:37 +0100

    Create Jenkins.sh

---
+++ Jenkins_server/Jenkins.sh
@@ -0,0 +1,28 @@
+sudo apt update
+hostnamectl set-hostname jenkins-server
+# Install Maven & Java
+sudo apt update
+sudo apt install default-jdk
+sudo apt update
+sudo apt install maven
+# Install and Start Jenkins 
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins
+systemctl enable jenkins
+systemctl start jenkins
+systemctl status jenkins
+# Install kubectl & eksctl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo touch /etc/apt/sources.list.d/kubernetes.list 
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+
+
