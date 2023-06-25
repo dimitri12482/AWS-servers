90ea9c0a5e2f719fc219fc894013d1fd53d878e1
Dimitri <65912918+dimitri12482@users.noreply.github.com>
2023-06-25 08:01:36 +0100

    Create variables.tf

---
+++ Jenkins_server/variables.tf
@@ -0,0 +1,15 @@
+variable "mykey" {
+  default = "devops"
+}
+variable "myami" {
+  default = "ami-0f8e81a3da6e2510a" #Latest Ubuntu
+}
+variable "instancetype" {
+  default = "t2.micro"
+}
+variable "tag" {
+  default = "Jenkins_Server1"
+}
+variable "jenkins-sg" {
+  default = "JenkinsSecGrp"
+}
